5fd89a1cdff4dc639354af8b3b555169f7b84275
Rebecca Reyes <rebreyaz@gmail.com>
2023-01-25 16:51:55 -0700

    add css

--- README.md
+++ README.md
@@ -37,13 +37,22 @@ Create a to-do-list web app.
 
 8) Inside the views folder, create a file called "list.ejs". This will be the base template to create all of the to-do-lists.
 
+9) Create a folder called public. Inside this folder create another folder called css with a file called styles.css
+
 ## Methods Used:
 * **getDay()** JavaScript method that returns the day of the week for the specified date according to local time, where 0 represents Sunday.
-* **res.send()** Express.js method that can only be called once. It Sends the HTTP response.
+
+* **toLocaleDateString()** JavaScript method that returns a string with a language-sensitive representation of the date portion of the specified date in the user agent's timezone.
+
 * **res.write()** Node.js method that can be called multiple times. The first time res.write() is called, it will send the buffered header information and the first chunk of the body to the client. The second time res.write() is called, Node.js assumes data will be streamed, and sends the new data separately. That is, the response is buffered up to the first chunk of the body.
+
+* **res.send()** Express.js method that can only be called once. It Sends the HTTP response.
+
 * **res.sendFile()** Express.js method that transfers the file at the given path. 
 
-* **res.render** Express.js method that renders a view and sends the rendered HTML string to the client. 
+* **res.redirect()** Express.js method that redirects to the URL derived from the specified path.
+
+
 
 ## Lessons Learned
 I learned how to:
@@ -51,3 +60,13 @@ I learned how to:
 ** use Express.js to build a web application and a server.
 
 * use Embedded JavaScript (EJS) templating to change certain parts of the HTML depending on the logic in my server.
+
+* use the scriptlet ejs tag to run some basic JavaScript code namely for control flow purposes, no output.
+
+* pass data from the ejs template to a server.
+
+* create an HTML form inside the ejs file that has a single text input that is going to take whatever value the user types in there as the next to do list item, and then the user hits the submit button that is going to make a POST request to the home route.
+
+** use an array to store items in a collection
+
+** add pre-made CSS stylesheets to a website.
--- app.js
+++ app.js
@@ -10,27 +10,50 @@ const https = require("https");
 // STEP 4: Create a constant called app and set it equal to the express() method.
 const app = express();
 
-// STEP 5: Use the app.set() method to set the app's view engine to ejs.
-app.set('view engine', 'ejs');
+// STEP 11: Create a variable called news and set it equal to an empty array
+// each time we get a new post request, and a new variable is created called item, then
+// we can append that item to our array called items.
+let items = [];
 
-// STEP 6: Run the server on port 3000
+// STEP 5: Use the app.set() method to set the app's view engine to ejs.
+app.set("view engine", "ejs");
+
+// STEP 6: Use the app.use() method to use the body-parser module and set urlencoded to the true setting.
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// STEP 7: Run the server on port 3000
 const port = 3000;
 
-// STEP 7: Use app.listen() and console.log() to check if server is running on given port.
+// STEP 8: Use app.listen() and console.log() to check if server is running on given port.
 app.listen(port, function () {
   console.log("Server is running on port " + port + ".");
 });
 
-// STEP 8: Create a GET request on the homepage route
+// STEP 9: Create a GET request on the homepage route
 app.get("/", function (req, res) {
-  var today = new Date();
-  var currentDay = today.getDay();
-  // if today is Saturday (6) or today is  Sunday (0)
-  if (currentDay === 6 || currentDay === 0) {
-    // then print the message "Yay, it's the weekend!"
-    res.send("<h1>Yay, it's the weekend!</h1>");
-  } else {
-    // otherwise, "Boo, it's the weekday. I have to work!"
-    res.send("<h1>Boo, it's the weekday. I have to work!</h1>");
-  }
+  const today = new Date();
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  const day = today.toLocaleDateString("eng-US", options);
+
+  //render our list.ejs passing in two variables: one called kindOfDay set to equal day and another
+  // called newListItems set to equal to the items array.
+  res.render("list", { kindOfDay: day, newListItems: items });
+});
+
+// STEP 10: Handle the post request to the homepage route
+app.post("/", function (req, res) {
+  // When a post request is triggered, save the value of newItem (input that user provided) to a
+  // variable called item and redirect to the homepage route which then sends you to app.get for the
+  // homepage route and it will res.render the list template passing in both the kindOfDay as well as the newListItem
+  let item = req.body.newItem;
+  // the object we want to push is the new item
+  items.push(item);
+
+  res.redirect("/");
 });
