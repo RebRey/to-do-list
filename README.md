e539c2dfbe3073d4aa20784c1389786d7eb23310
Rebecca Reyes <rebreyaz@gmail.com>
2023-02-10 12:03:22 -0700

    remove delete buttons

--- README.md
+++ README.md
@@ -4,40 +4,42 @@ Stay organized and manage your day-to-day tasks with this to-do-list web app.
 ## Objective:
 Create a to-do-list web app.
 
-## Initial Set-Up:
+## Set-Up:
 1) Create the following files:
-* index.html
 * app.js
-2) cd into the current project folder and initialize npm using the command line interface. This will create a package.json file.
+* date.js
+* index.html
+
+2) Create the following folders and files:
+* public (folder) > css (folder) > styles.css (file)
+* views (folder) > about.ejs, footer.ejs, header.ejs, list.ejs
+
+## Modules:
+On the Command Line Interface (CLI) cd into your project and do the following:
+1) Initialize npm. Follow the prompts on the CLI. This will create a package.json file.
 ```
   $ npm init
 ```
 
-3) Install npm in the current project folder. This will create a package-lock.json file. 
+2) Install npm in the current project folder. This will create a package-lock.json file. 
 ```
   $ npm install
 ```
 
-4) Use npm to install express in the current project folder. You can check that it is installed in the package.json file under dependencies. 
+3) Use npm to install express in the current project folder. You can check that it is installed in the package.json file under dependencies. 
 ```
   $ npm install express
 ```
 
-5) Use npm to install body-parser in the current project folder.You can check that it is installed in the package.json file under dependencies. 
+4) Use npm to install body-parser in the current project folder.You can check that it is installed in the package.json file under dependencies. 
 ```
   $ npm install body-parser
 ```
 
-6) Use npm to install ejs in the current project folder. You can check that it is installed in the package.json file under dependencies. 
+5) Use npm to install ejs in the current project folder. You can check that it is installed in the package.json file under dependencies. 
 ```
   $ npm install ejs
 ```
-
-7) Create a folder called "views". This is where the view engine by default will go and look for the files that need rendering. 
-
-8) Inside the views folder, create a file called "list.ejs". This will be the base template to create all of the to-do-lists.
-
-9) Create a folder called public. Inside this folder create another folder called css with a file called styles.css
 
 ## Methods Used:
 * **getDay()** JavaScript method that returns the day of the week for the specified date according to local time, where 0 represents Sunday.
@@ -53,7 +55,6 @@ Create a to-do-list web app.
 * **res.redirect()** Express.js method that redirects to the URL derived from the specified path.
 
 
-
 ## Lessons Learned
 I learned how to:
 
@@ -67,8 +68,8 @@ I learned how to:
 
 * create an HTML form inside the ejs file that has a single text input that is going to take whatever value the user types in there as the next to do list item, and then the user hits the submit button that is going to make a POST request to the home route.
 
-** use an array to store items in a collection
+** use an array to store items in a collection.
 
 ** add pre-made CSS stylesheets to a website.
 
-** pass functions and data between files by creating local modules and exporting them with node.js
+** pass functions and data between files by creating local modules and exporting them with node.js.
--- app.js
+++ app.js
@@ -1,44 +1,37 @@
-// STEP 1: Require the express module. (also install using CLI)
+// Require the express module. (also install using CLI)
 const express = require("express");
 
-// STEP 2: Require the body-parser module  (also install using CLI)
+// Require the body-parser module  (also install using CLI)
 const bodyParser = require("body-parser");
 
-// STEP 3: Require the https module.
+// Require the https module.
 const https = require("https");
+
+// Create a constant called app and set it equal to the express() method
+const app = express();
+
+// Run the server on port 3000
+const port = 3000;
 
 // Require the date module we created (this is a local module so use __dirname)
 const date = require(__dirname + "/date.js");
 
-// STEP 4: Create a constant called app and set it equal to the express() method.
-const app = express();
-
-// STEP 11: Create a variable called news and set it equal to an empty array
-// each time we get a new post request, and a new variable is created called item, then
-// we can append that item to our array called items.
-// you can push items into an array, you can use the const for declaring the variable
+// Create a variable called news and set it equal to an empty array
 const items = [];
 
-// Step 14:
+// Create a variable called workItems and set it qual to an empty array
 const workItems = [];
-// STEP 5: Use the app.set() method to set the app's view engine to ejs.
+
+// Use the app.set() method to set the app's view engine to ejs.
 app.set("view engine", "ejs");
 
-// STEP 6: Use the app.use() method to use the body-parser module and set urlencoded to the true setting.
+// Use the app.use() method to use the body-parser module and set urlencoded to the true setting.
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// STEP 12: Use the app.use() method and express.static() methods to serve static files (CSS style sheet) in Express.
+// Use the app.use() method and express.static() methods to serve static files (CSS style sheet) in Express.
 app.use(express.static("public"));
 
-// STEP 7: Run the server on port 3000
-const port = 3000;
-
-// STEP 8: Use app.listen() and console.log() to check if server is running on given port.
-app.listen(port, function () {
-  console.log("Server is running on port " + port + ".");
-});
-
-// STEP 9: Create a GET request on the homepage route
+// Create a GET request on the homepage route
 app.get("/", function (req, res) {
   // create a variable called day and set it equal to a function call for date() that is bound to the const date
   // to activate getDate() function
@@ -49,42 +42,30 @@ app.get("/", function (req, res) {
   res.render("list", { listTitle: day, newListItems: items });
 });
 
-// STEP 10: Handle the post request to the homepage route
+// Handle the post request to the homepage route
 app.post("/", function (req, res) {
-  // When a post request is triggered, save the value of newItem (input that user provided) to a
-  // variable called item
   const item = req.body.newItem;
 
-  //
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
   } else {
-    // redirect to the homepage route which then sends you to app.get for the
-    // homepage route and it will res.render the list template passing in both the kindOfDay as well as the newListItem
-    // the object we want to push is the new item
     items.push(item);
     res.redirect("/");
   }
 });
 
-// Step 13: set a GET request for the work page
+// Create a GET request for the work page
 app.get("/work", function (req, res) {
   res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-// STEP 15: set a GET request for the about page
+// Create a GET request for the about page
 app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.post("/deleteItem", function (req, res){
-  const item = req.body.newItem;
-  if(req.body.list === "Work"){
-      workItems.splice(workItems.indexOf(item), 1);
-      res.redirect("/work");
-  }else{
-      items.splice(items.indexOf(item), 1);
-      res.redirect("/");
-  }
+// Use app.listen() and console.log() to check if server is running on given port.
+app.listen(port, function () {
+  console.log("Server is running on port " + port + ".");
 });
